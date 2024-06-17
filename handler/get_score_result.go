fec49a43c5b302e701911ee105c550d5ed7d0da5
shahao <shahao@bytedance.com>
2024-06-17 12:55:17 +0800

    reset rawdata

--- handler/get_score_result.go
+++ handler/get_score_result.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"math"
 	"sort"
 	"teamup/constant"
@@ -65,14 +67,16 @@ type GetScoreResultResp struct {
 //	@Success		200					{object}	GetScoreResultResp
 //	@Router			/team_up/user/get_score_result [post]
 func GetScoreResult(c *model.TeamUpContext) (interface{}, error) {
-	//raw, err := c.GetRawData()
-	//if err != nil {
-	//	util.Logger.Printf("[GetScoreResult] get raw data failed, err:%v", err)
-	//	return nil, iface.NewBackEndError(iface.InternalError, "get raw data failed")
-	//}
-	//util.Logger.Println("[GetScoreResult] starts, rawData:%v", string(raw))
+	raw, err := c.GetRawData()
+	if err != nil {
+		util.Logger.Printf("[GetScoreResult] get raw data failed, err:%v", err)
+		return nil, iface.NewBackEndError(iface.InternalError, "get raw data failed")
+	}
+	util.Logger.Printf("[GetScoreResult] starts, rawData:%v", string(raw))
+	// 重置请求体供后续解析
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(raw))
 	body := &UploadRoundInfos{}
-	err := c.BindJSON(body)
+	err = c.BindJSON(body)
 	if err != nil {
 		util.Logger.Printf("[GetScoreResult] bindJSON failed, err:%v", err)
 		return nil, iface.NewBackEndError(iface.ParamsError, "bindJSON failed")
